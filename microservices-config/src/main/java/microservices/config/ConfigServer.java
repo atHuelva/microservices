0649575513c1141e8adb4329448441da4ef2e177
Hector Mendez <hjmendez@atsistemas.com>
2018-11-28 11:29:19 +0100

    configuración de spring cloud

--- microservices-config/src/main/java/microservices/config/ConfigServer.java
+++ microservices-config/src/main/java/microservices/config/ConfigServer.java
@@ -8,9 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ConfigServer.class, args);
-  }
-
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer.class, args);
+	}
 
 }
