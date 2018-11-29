3ae6da8ec20b73c7939ef115bfcb30e7b90a133b
ATSISTEMAS\apena.castro <apena.castro@atsistemas.com>
2018-11-29 09:54:06 +0100

    Ampliado fallback con HystrixTimeoutException

--- microservices-client/src/main/java/microservices/client/fallback/HystrixClientFallback.java
+++ microservices-client/src/main/java/microservices/client/fallback/HystrixClientFallback.java
@@ -1,5 +1,7 @@
 package microservices.client.fallback;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+
 import microservices.client.dto.GreetingsDto;
 import microservices.client.dto.GreetingsPhpDto;
 import microservices.client.service.GreetingsFeignClient;
@@ -16,13 +18,24 @@ public class HystrixClientFallback implements GreetingsPhpFeignClient, Greetings
 	@Override
 	public GreetingsPhpDto getSaludos() {
 		GreetingsPhpDto failObject = new GreetingsPhpDto();
-		failObject.setName(String.format("Microservice not available. Reason: %s", cause.getMessage()));
+		if (cause instanceof HystrixTimeoutException) {
+			failObject.setName("Microservice not available. Timeout received");
+		} else {
+			failObject.setName(String.format("Microservice not available. Reason: %s", cause.getMessage()));
+		}
 		return failObject;
 	}
 
 	@Override
 	public GreetingsDto getGreetings(String name) {
-		return new GreetingsDto(String.format("Microservice not available. Reason: %s", cause.getMessage()));
+		GreetingsDto failObject;
+
+		if (cause instanceof HystrixTimeoutException) {
+			failObject = new GreetingsDto("Microservice not available. Timeout received");
+		} else {
+			failObject = new GreetingsDto(String.format("Microservice not available. Reason: %s", cause.getMessage()));
+		}
+		return failObject;
 	}
 
 }
