face161c55d7d02f27c40712fa8121cbdab492ea
Hector Mendez <hjmendez@atsistemas.com>
2018-11-29 11:56:34 +0100

    Configuración del zuul

--- microservices-zuul/src/main/java/microservices/zuul/ZuulApplication.java
+++ microservices-zuul/src/main/java/microservices/zuul/ZuulApplication.java
@@ -2,26 +2,43 @@ package microservices.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-import feign.RequestInterceptor;
+import microservices.zuul.filter.ErrorFilter;
+import microservices.zuul.filter.PostFilter;
+import microservices.zuul.filter.PreFilter;
+import microservices.zuul.filter.RouteFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableOAuth2Sso
 public class ZuulApplication {
+	
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
 	}
 
 	@Bean
-	public RequestInterceptor getUserFeignClientInterceptor() {
-		return new UserFeignClientInterceptor();
+	public PostFilter postFilter() {
+		return new PostFilter();
 	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
 }
